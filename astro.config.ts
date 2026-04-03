aa908305f622970dd6632c029b4d31aa2ee7eaa7
kngwyu <yuji.kngw.80s.revive@gmail.com>
2026-04-03 19:03:22 +0900

    Fix github pages

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,8 @@ import unocss from 'unocss/astro';
 
 export default defineConfig({
   // In 2026, GitHub Pages needs this if your repo isn't 'username.github.io'
+  site: 'https://astronaut.github.io',
+  base: 'fear-prey-predator-gecco2026',
   integrations: [
     react(),
     unocss({
